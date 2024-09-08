199fd8a9342cab8c2ce69400063c4c991775356d
jarrenleo <jarren.leoo@gmail.com>
2024-09-08 21:53:04 +0800

    added react query

--- app/_components/upcoming/UpcomingList.js
+++ app/_components/upcoming/UpcomingList.js
@@ -1,49 +1,46 @@
 "use client";
 
-import { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 import UpcomingListItem from "./UpcomingListItem";
 import Spinner from "../others/Spinner";
 
+async function fetchUpcomingList(channel, country, timeZone) {
+  const response = await fetch(
+    `/api/upcoming?channel=${channel}&country=${country}&timeZone=${timeZone}`,
+  );
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const data = await response.json();
+  return data;
+}
+
 export default function UpcomingList() {
   const searchParams = useSearchParams();
-  const [data, setData] = useState([]);
-
   const channel = searchParams.get("channel");
   const country = searchParams.get("country");
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  useEffect(() => {
-    (async function () {
-      try {
-        const response = await fetch(
-          `/api/upcoming?channel=${channel}&country=${country}&timeZone=${timeZone}`,
-        );
-        if (!response.ok) throw new Error("Failed to fetch data");
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["UpcomingList", channel, country, timeZone],
+    queryFn: () => fetchUpcomingList(channel, country, timeZone),
+  });
 
-        const data = await response.json();
-
-        setData(data);
-      } catch (error) {
-        setError(error.message);
-      }
-    })();
-  }, [channel, country, timeZone]);
+  if (isPending) return <Spinner />;
+  if (isError) return <div>{error.message}</div>;
 
   return (
-    <Suspense fallback={<Spinner />}>
-      <div className="h-[calc(100dvh-11rem)] overflow-y-scroll pt-2">
-        {Object.entries(data).map(([date, products]) => {
-          return (
-            <ul key={date} className="mb-2">
-              <li className="px-2 py-1 text-sm">{date}</li>
-              {products.map((product) => (
-                <UpcomingListItem key={product.id} product={product} />
-              ))}
-            </ul>
-          );
-        })}
-      </div>
-    </Suspense>
+    <div className="h-[calc(100dvh-11rem)] overflow-y-scroll pt-2">
+      {Object.entries(data).map(([date, products]) => {
+        return (
+          <ul key={date} className="mb-2">
+            <li className="px-2 py-1 text-sm">{date}</li>
+            {products.map((product) => (
+              <UpcomingListItem key={product.id} product={product} />
+            ))}
+          </ul>
+        );
+      })}
+    </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
-import { Courier_Prime } from "next/font/google";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
+import QueryProvider from "./_providers/QueryProvider";
 import ThemeProvider from "./_providers/ThemeProvider";
-
-const courierPrime = Courier_Prime({
-  weight: ["400"],
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Sneakify",
@@ -15,15 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${courierPrime.className}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+      <body className={`${GeistMono.className}`}>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
