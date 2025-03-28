502cb542176a56dc10d72a659067f6f748bd61d9
jarrenleo <jarren.leoo@gmail.com>
2025-03-28 20:57:34 +0800

    styling changes

--- app/_components/navigation/SearchResult.js
+++ app/_components/navigation/SearchResult.js
@@ -54,14 +54,14 @@ export default function SearchResult({
 
   if (isPending && searchQuery.length > 9)
     return (
-      <div className="absolute top-[50px] z-10 flex w-full justify-center rounded-md bg-muted p-4">
+      <div className="absolute top-[50px] z-10 flex w-full justify-center rounded-md bg-muted py-4">
         <Spinner size={30} stroke={3} />
       </div>
     );
 
   if (error)
     return (
-      <div className="absolute top-[50px] z-10 w-full rounded-md bg-muted py-8 text-center text-sm text-muted-foreground">
+      <div className="absolute top-[50px] z-10 w-full rounded-md bg-muted py-4 text-center text-sm text-muted-foreground">
         {error.message}
       </div>
     );
@@ -71,7 +71,7 @@ export default function SearchResult({
       <Link
         href={`/${data.sku}`}
         onClick={() => handleClick(data.channel)}
-        className="absolute top-[50px] z-10 flex w-full cursor-pointer items-center gap-2 rounded-md bg-muted p-4 transition-colors hover:bg-neutral-700"
+        className="absolute top-[50px] z-10 flex w-full cursor-pointer items-center gap-2 rounded-md bg-muted px-3 py-2 transition-colors hover:bg-neutral-700 md:px-4 md:py-2"
       >
         <div className="h-10 w-10 flex-shrink-0">
           <img
