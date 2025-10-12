395ce146746127aed30458f58e2146fe541ce3db
jarrenleo <jarren.leoo@gmail.com>
2025-10-12 15:56:19 +0800

    updates

--- app/[country]/page.js
+++ app/[country]/page.js
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
 import Upcoming from "@/app/_components/upcoming/Upcoming";
+
+const countries = ["AU", "JP", "KR", "SG", "MY", "FR", "GB", "CA", "US", "MX"];
 
 export async function generateMetadata({ params }) {
   const { country } = await params;
@@ -8,6 +11,10 @@ export async function generateMetadata({ params }) {
   };
 }
 
-export default function Page() {
+export default async function Page({ params }) {
+  const { country } = await params;
+
+  if (!countries.includes(country.toUpperCase())) return redirect("/SG");
+
   return <Upcoming />;
 }
--- app/_components/upcoming/Upcoming.js
+++ app/_components/upcoming/Upcoming.js
@@ -7,14 +7,10 @@ import TabSelect from "./TabSelect";
 import UpcomingList from "./UpcomingList";
 import CountrySelect from "./CountrySelect";
 
-const countries = ["AU", "JP", "KR", "SG", "MY", "FR", "GB", "CA", "US", "MX"];
-
 export default function Upcoming() {
   const router = useRouter();
   const { country } = useParams();
   const { setCountry } = useGlobalState();
-
-  if (!countries.includes(country.toUpperCase())) return redirect("/SG");
 
   useEffect(() => {
     setCountry(country.toUpperCase());
